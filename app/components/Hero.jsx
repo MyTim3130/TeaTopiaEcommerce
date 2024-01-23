d79989b76f93123245f9ee30e0f9ac94cb09c1c7
MyTim3130 <tin.hauslkramper@gmail.com>
2024-01-23 06:27:26 +0100

    secondCommit

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -4,15 +4,43 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <>
-      <section>
-      <Image
+      <section className="h-[100vh]">
+        <Image
           className="w-auto h-screen object-cover sm:w-screen absolute z-[-1]"
           src="/assets/background1.png"
           width={10000}
           height={10000}
-          
         />
-        <h1 className="text-white text-5xl flex justify-center items-center h-72 font-extrabold bg-gradient-to-r from-lime-100 to-yellow-300 bg-clip-text text-transparent sm:text-8xl">TeaTopia</h1>
+        <h1 className="text-white text-5xl flex justify-center items-center h-72 font-extrabold  sm:text-8xl">
+          TeaTopia
+        </h1>
+        <div className="w-screen h-fit flex items-center justify-center text-center">
+          <p className="w-[70vw] text-2xl text-white relative bottom-16 sm:text-3xl sm:bottom-11">
+            Willkommen bei TeaTopia <br />
+            Entdecken Sie eine Welt voller exquisiter Tees!
+          </p>
+        </div>
+      </section>
+
+      <Image
+        className="w-auto h-auto object-cover sm:w-screen absolute z-[0] top-[101vh] sm:top-[88vh]"
+        src="/assets/liana.png"
+        width={10000}
+        height={10000}
+      />
+      <section className="h-screen flex flex-col items-center">
+        <div className="w-full h-fit">
+          <div className="w-full h-fit">
+            <Image
+              className="w-52 h-auto object-cover sm:w-screen absolute z-[0]"
+              src="/assets/kannen.png"
+              width={10000}
+              height={10000}
+            />
+          </div>
+
+          <div></div>
+        </div>
       </section>
     </>
   );
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Nav = () => {
   return (
     <>
-      <section className="bg-[#adbda7] h-[6vh] sm:h-[9vh] fixed w-screen">
+      <section className="bg-[#adbda7] h-[6vh] sm:h-[9vh] fixed w-screen z-50">
         <div className="flex items-center justify-around h-full">
           <div>
             <Image className="h-10 w-auto rounded-full sm:h-16 sm:w-auto" src="/assets/logo.png" width={1000} height={1000} />
