66ffd74544b148fb21889ece8fe2e9b7cffd60fa
MyTim3130 <tin.hauslkramper@gmail.com>
2024-01-24 06:10:20 +0100

    vercel

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -23,24 +23,56 @@ const Hero = () => {
       </section>
 
       <Image
-        className="w-auto h-auto object-cover sm:w-screen absolute z-[0] top-[101vh] sm:top-[88vh]"
+        className="w-auto h-auto object-cover sm:w-screen absolute z-[0] top-[101vh] xl:top-[85vh] lg:top-[95vh]"
         src="/assets/liana.png"
         width={10000}
         height={10000}
       />
-      <section className="h-screen w-screen flex flex-col items-center">
-      <div className="w-sceen h-auto flex justify-center">
-        <h2 className="text-3xl sm:text-5xl sm:mt-28">Verschiedenste Sorten!</h2>
-          
-            <Image
-              className="w-64 h-auto object-cover absolute z-[0] mt-16 sm:h-[80vh] sm:w-auto sm:mt-36"
-              src="/assets/kannen.png"
-              width={10000}
-              height={10000}
-            />
-          
+      <section className="h-screen w-screen flex flex-col">
+        <Image
+          className="w-auto h-screen object-cover sm:w-[110vw] absolute z-[-1] top-[106vh]"
+          src="/assets/bg3.png"
+          width={10000}
+          height={10000}
+        />
+        <div className="w-sceen h-auto flex items-center flex-col">
+          <h2 className="text-3xl text-white sm:text-5xl sm:mt-0">
+            Verschiedenste Sorten!
+          </h2>
 
-          <div></div>
+          <Image
+            className="w-64 h-auto object-cover absolute z-[0] mt-16 sm:h-[80vh] sm:w-auto sm:mt-0"
+            src="/assets/kannen.png"
+            width={10000}
+            height={10000}
+          />
+
+          <div className="w-screen h-96 bg-lime-950 mt-96 sm:mt-[35rem] sm:h-[50vh] grid grid-cols-2">
+            <div className="h-50% flex mt-4 items-center flex-col">
+              <span className="text-white text-2xl sm:text-4xl">Vegan</span>
+              <div>
+                <Image src='/assets/vegan.png' className="mt-10 h-20 w-auto sm:h-28 sm:mt-5" width={500} height={500} />
+              </div>
+            </div>
+            <div className="h-50% flex mt-4 items-center flex-col">
+              <span className="text-white text-2xl sm:text-4xl">Price</span>
+              <div>
+                <Image src='/assets/increase.png' className="mt-10 h-20 w-auto sm:h-28 sm:mt-5" width={500} height={500} />
+              </div>
+            </div>
+            <div className="h-50% flex mt-4 items-center flex-col">
+              <span className="text-white text-2xl sm:text-4xl">Health</span>
+              <div>
+                <Image src='/assets/healthcare.png' className="mt-10 h-20 w-auto sm:h-28 sm:mt-5" width={500} height={500} />
+              </div>
+            </div>
+            <div className="h-50% flex mt-4 items-center flex-col">
+              <span className="text-white text-2xl sm:text-4xl">Taste</span>
+              <div>
+                <Image src='/assets/taste.png' className="mt-10 h-20 w-auto sm:h-28 sm:mt-5" width={500} height={500} />
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
